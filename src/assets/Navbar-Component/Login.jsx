a904d1b171beb6c77cb6d5a6db996235b3cdd24b
iamfaisalpk <iamfaizy123@gmail.com>
2025-03-04 21:13:31 +0530

    five commit

--- src/assets/Navbar-Component/Login.jsx
+++ src/assets/Navbar-Component/Login.jsx
@@ -89,19 +89,19 @@ const Login = () => {
 
                     <p className="text-xl text-white mt-3">
                         Don't have an account?
-                        <Link to="/register">
+                        <Link to="/reg">
                             <button className="cursor-pointer text-xl hover:text-black">
                                 Sign up
                             </button>
                         </Link>
                     </p>
 
-                    <button
+                    {/* <button
                         onClick={handleLogout}
                         className="text-white cursor-pointer bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg mt-3"
                     >
                         Logout
-                    </button>
+                    </button> */}
                 </div>
             </div>
         </>
