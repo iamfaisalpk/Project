095bc38215138ab97541f02773d2148712264dba
iamfaisalpk <iamfaizy123@gmail.com>
2025-03-21 16:43:15 +0530

    fifth commit

--- src/assets/Admin/AdminLayout.jsx
+++ src/assets/Admin/AdminLayout.jsx
@@ -3,7 +3,6 @@ import AdminSidebar from './AdminSidebar';
 import Header from '../Header';
 
 
-
 const AdminLayout = ({ children }) => {
   return (
     <div className="flex min-h-screen bg-gray-100">
