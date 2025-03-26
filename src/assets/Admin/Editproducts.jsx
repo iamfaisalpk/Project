41e92cff4a7dad6eabfd1a16bb079901889e1641
iamfaisalpk <iamfaizy123@gmail.com>
2025-03-26 15:07:38 +0530

    second commit

--- src/assets/Admin/Editproducts.jsx
+++ src/assets/Admin/Editproducts.jsx
@@ -172,7 +172,7 @@ return (
                 src={product.imageUrl}
                 alt="Product preview"
                 className="max-w-xs h-auto rounded-lg shadow-md"
-                  onError={(e) => (e.target.style.display = 'none')} // Hide broken images
+                  onError={(e) => (e.target.style.display = 'none')} 
                 />
             </div>
             )}
